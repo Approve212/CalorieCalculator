27c2ae1b7014368902d60213a72dc00e5d7d7d56
Caleb Kimberlin <cmkimberlin@gmail.com>
2021-11-30 21:14:28 -0600

    Added calcCalorie method and button to activate method

--- Form1.cs
+++ Form1.cs
@@ -10,12 +10,34 @@ using System.Windows.Forms;
 
 namespace CalorieCalculator
 {
+    public class Food
+    {
+        int protein, carb, fat;
+    }
+
     public partial class Form1 : Form
     {
+        
+        double calcCalorie(Food food)
+        {
+            double proteinCal, carbCal, fatCal, foodCal;
+
+            proteinCal = food.protein * 4;
+            carbCal = food.carb * 4;
+            fatCal = food.fat * 9;
+
+            foodCal = proteinCal + carbCal + fatCal;
+            return foodCal;
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            calcCalorie(food);
+        }
     }
 }
